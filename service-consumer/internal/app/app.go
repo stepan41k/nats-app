9fe98a1bd5f5f03a8722b46f5560e8b9774c978f
stepan41k <russia.zont@gmail.com>
2025-12-26 05:02:41 +0300

    structure: create initial structure

---
+++ service-consumer/internal/app/app.go
@@ -0,0 +1,10 @@
+package app
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"net/http"
+// 	"time"
+
+// 	pb "github.com/stepan41k/GinTest/service-1/pkg/api/v1"
+// )
---
+++ service-producer/go.mod
@@ -0,0 +1,3 @@
+module github.com/stepan41k/GinTest/service-1
+
+go 1.25.3
